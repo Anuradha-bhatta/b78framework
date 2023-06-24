a08b847785d5ad79d701dc586ca82f054dc86d24
Anuradha <Anuradha@BQ0R1J3-OOBE-01>
2023-06-24 07:54:11 +0530

    Ist time commit-done with akshara

---
+++ src/test/java/generic/BaseTest.java
@@ -0,0 +1,40 @@
+package generic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
+public class BaseTest {
+	public final String DEFAULT_URL="http://www.google.com";
+	public final String DEFAULT_ITO="6";
+	public final String DEFAULT_ETO="7";
+	public WebDriver driver;
+	public WebDriverWait wait;
+	
+	@Parameters({"appURL","ITO","ETO"})
+	@BeforeMethod
+	public void preCondition(@Optional(DEFAULT_URL) String appURL,@Optional(DEFAULT_ITO) String ITO,@Optional(DEFAULT_ETO) String ETO)
+	{
+		long longITO=Long.parseLong(ITO);
+		long longETO=Long.parseLong(ETO);
+		
+		driver=new ChromeDriver();
+		driver.get(appURL);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(longITO));
+		wait=new WebDriverWait(driver, Duration.ofSeconds(longETO));
+	}
+	
+	
+	@AfterMethod
+	public void postCondition()
+	{
+		driver.close();
+	}
+}
